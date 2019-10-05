67f3b7c421ec4ba2c602afaaf28b1be3dec28657
yaowenqiang <yaowenqiang111@163.com>
2019-10-05 22:30:17 +0800

    使用智能指针实现递归类型长度计划

--- add-two/src/main.rs
+++ add-two/src/main.rs
@@ -1,4 +1,29 @@
+use crate::List::{Cons, Nil};
+
+/*
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+*/
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
 fn main() {
     let b = Box::new(5);
-    println!("b = {}", b)
+    println!("b = {}", b);
+
+    //let list = Cons(3, Cons(2, Cons(3, Nil)));
+    
+    let list = Cons(1, 
+        Box::new(Cons(2, 
+            Box::new(Cons(3, 
+                Box::new(Nil))))));
+
+    println!("{:?}", list)
+
+    
+
 }
