2537ced45711bc6cb077f7e4fed6eb15a824fa57
Yaowenqiang <yaowenqiang111@163.com>
2019-10-13 22:01:31 +0800

    add 404

--- server/src/main.rs
+++ server/src/main.rs
@@ -17,11 +17,23 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
     println!("Request : {}", String::from_utf8_lossy(&buffer[..]));
-    let contents = fs::read_to_string("index.html").unwrap();
 
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
+
+        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n{}", contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+        println!("Request : {}", String::from_utf8_lossy(&buffer[..]));
+    }
 
 
 
