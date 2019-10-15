6c41bc48ff67f1e229420f74e025ac387d4d2b1d
Yaowenqiang <yaowenqiang111@163.com>
2019-10-15 21:41:33 +0800

    add threads to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,12 +7,12 @@ use std::fs;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
-
-
         let stream =  stream.unwrap();
-        handle_connection(stream);
-    }
 
+        thread::spawn( ||  {
+            handle_connection(stream);
+        });
+    }
 }
 
 fn handle_connection(mut stream: TcpStream) {
