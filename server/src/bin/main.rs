13d1f693ea5c5bcc07cb2b2f01c237c08b5775dd
Yaowenqiang <yaowenqiang111@163.com>
2019-10-15 22:17:15 +0800

    add ThreadPool

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -4,12 +4,16 @@ use std::net::TcpListener;
 use std::thread;
 use std::time::Duration;
 use std::fs;
+
+use server::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream =  stream.unwrap();
 
-        thread::spawn( ||  {
+        pool.execute( ||  {
             handle_connection(stream);
         });
     }
