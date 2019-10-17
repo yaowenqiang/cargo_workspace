a10a33627df5d6c3207172dc37dd79a8fa21947c
Yaowenqiang <yaowenqiang111@163.com>
2019-10-17 21:39:28 +0800

    change thread to worker

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -9,7 +9,7 @@ use server::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("227.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(0);
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream =  stream.unwrap();
 
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,6 @@
 use std::thread;
 pub struct ThreadPool{
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -11,17 +11,32 @@ impl ThreadPool {
 
     pub fn new (size: usize) -> ThreadPool {
         assert!(size > 0);
-        let mut threads = Vec::with_capacity(size);
-        for _ in 0..size {
-
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id));
         }
         ThreadPool {
-            threads
+            workers
         }
     }
 
     pub fn execute<F>(&self, f:F)  
         where F:FnOnce() + Send  + 'static {
 
+    }
+}
+
+struct Worker {
+    id: usize, 
+    thread: thread::JoinHandle<()>,
+}
+
+impl  Worker {
+     fn new(id: usize) -> Worker{
+        let thread = thread::spawn(|| {}) ;
+        Worker {
+            id,
+            thread
+        }
     }
 }
