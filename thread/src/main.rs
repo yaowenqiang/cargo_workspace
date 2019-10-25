99ec077a8950a47898bb5c484ef2b994d38aa937
Yaowenqiang <yaowenqiang111@163.com>
2019-10-25 22:59:21 +0800

    add thread use main thread variables demo

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -2,9 +2,12 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn(move || {
         for i in 1 .. 10 {
             println!("hi, number {} from the spawned thread!", i);
+            println!("Here's a vector {:?}", v);
             thread::sleep(Duration::from_millis(1));
         }
     });
