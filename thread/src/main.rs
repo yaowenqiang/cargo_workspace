e5bda317a5cf3b27bebde2a00fa6d3cfc88e4efa
Yaowenqiang <yaowenqiang111@163.com>
2019-10-27 21:55:31 +0800

    add channel demo

--- thread/src/main.rs
+++ thread/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     let mut v =  vec![1,2,3];
@@ -20,4 +21,25 @@ fn main() {
         //thread::sleep(Duration::from_millis(1));
     }
         //handle.join().unwrap();
+    let (tx, rx) = mpsc::channel();
+    // tx.send(()).unwrap();
+    thread::spawn(move || {
+        let val = String::from("Hi");
+        tx.send(val).unwrap();
+    });
+
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
+}
+
+fn bad_thread_usage() {
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn( move || {
+        println!("Here's a vector: {:?}", v); 
+    });
+
+    //drop(v);
+    handle.join().unwrap();
+
 }
