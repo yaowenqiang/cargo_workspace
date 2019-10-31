bb63560508a6a66857e14ef377bd3a284b0e210c
Yaowenqiang <yaowenqiang111@163.com>
2019-10-31 23:17:13 +0800

    add nusafe function demo

--- unsafe_demo/src/main.rs
+++ unsafe_demo/src/main.rs
@@ -12,4 +12,25 @@ fn main() {
 
 
     println!("Hello, world!");
+
+    unsafe {
+        unsafe_function();
+    }
+}
+
+unsafe fn unsafe_function() {
+    let mut num = 5;
+    let r1 = &num as *const i32;
+    let r2 = &mut num as *mut i32;
+    let address = 0x012345usize;
+    let r = address as *const i32;
+    
+    //unsafe {
+        println!("r1 is: {} ", *r1);
+        println!("r2 is: {} ", *r2);
+    //}
+
+
+    println!("Hello, world!");
+
 }
