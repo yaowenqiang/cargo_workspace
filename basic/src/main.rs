02d680469a8f66b4cf333e1924666cc22eb56c3f
Yaowenqiang <yaowenqiang111@163.com>
2019-11-12 22:57:57 +0800

    add string slice demo

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-        let heart_eyed_cat = '😻';
-        println!("can you see a cat ?{}", heart_eyed_cat);
+    let heart_eyed_cat = '😻';
+    println!("can you see a cat ?{}", heart_eyed_cat);
+    {
+    let s = "hello";
+    println!("{}", s);
+    let mut st = String::from("hello");
+    st.push_str(", world");
+    println!("{}", st);
+    }
+    
+    //println!("{}", s);
 
 }
