2ca60b435da795ec97f7625e839db838f4178ac2
Yaowenqiang <yaowenqiang111@163.com>
2019-11-29 22:25:58 +0800

    add vector demo

---
+++ collections/src/main.rs
@@ -0,0 +1,11 @@
+
+fn main() {
+    let mut v : Vec<i32>  = Vec::new();
+
+    println!("{:?}", v);
+    v.push(33);
+    println!("{:?}", v);
+
+    let vv = vec!["abc", "def"];
+    println!("{:?}", vv);
+}
