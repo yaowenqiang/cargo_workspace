c66dc727b3c43b55e755b48a4a244761941b71c3
Yaowenqiang <yaowenqiang111@163.com>
2019-11-30 21:59:01 +0800

    add more vector demo and methds to ge vector elements

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
 
     let vv = vec!["abc", "def"];
     println!("{:?}", vv);
+
+
+    let vvi = vec![1,2,3,4];
+    let third: &i32 = &vvi[2];
+    println!("{}", third);
+
+    match vvi.get(2) {
+        Some(third) => println!("{}", third),
+        None => println!("there is not third element")
+    }
+
+
 }
