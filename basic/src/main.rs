6f2273584c8308c305e99c6675cb361a0c7cd416
Yaowenqiang <yaowenqiang111@163.com>
2019-12-04 23:03:40 +0800

    add reference and destructuring demo

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -125,6 +125,31 @@ fn main() {
     println!("{}", string_literal);
 
 
+
+    let reference = &4;
+    match reference {
+        &val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    match *reference {
+        val => println!("Got a value via destructuring: {:?}", val),
+    }
+
+    let ref _is_a_reference = 4;
+
+    let value = 5;
+    let mut mut_value = 6;
+    match value {
+        ref r => println!("Got a reference from a value : {:?}", r),
+    }
+
+    match mut_value {
+        ref mut m => {
+            *m += 30;
+            println!("We add 30 , 'mut_value' : {:?}", m)
+        }
+    }
+
     
 }
 /*
@@ -198,4 +223,7 @@ fn used_function() {
 #[allow(dead_code)]
 fn unused_function() {
 }
+
+
+
 
