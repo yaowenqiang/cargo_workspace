744590f9b81d507ca537fd60f1e0b393b40df12b
Yaowenqiang <yaowenqiang111@163.com>
2019-12-13 23:14:41 +0800

    add crate vec with different type via enum demo

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -80,6 +80,22 @@ fn main() {
 
     println!("{:?}", scores);
 
+    #[derive(Debug)]
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String)
+    }
+
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("Blue")),
+        SpreadsheetCell::Float(10.12)
+    ];
+    
+    println!("{:?}", row);
+
+
 
 
 
