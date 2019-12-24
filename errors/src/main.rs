2754506b35afb65f0ed45e8075b8891709e6e9b0
Yaowenqiang <yaowenqiang111@163.com>
2019-12-24 22:48:46 +0800

    add  error handle demo

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,7 +1,16 @@
+use std::fs::File;
 fn main() {
     //panic!("Crash and burn");
 
     let v = vec![1,2,3];
-    v[90];
+    //v[90];
+
+    let f  = File::open("foo");
+    let f = match f {
+        Ok(file) =>  file,
+        Err(error) => {
+            panic!("failed to open the file {:?}", error)
+        } 
+    };
 
 }
