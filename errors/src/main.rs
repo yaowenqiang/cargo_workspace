ebcb5671ed567a475325d0f20070851de315c4f2
Yaowenqiang <yaowenqiang111@163.com>
2019-12-25 23:10:05 +0800

    add error match demo

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 fn main() {
     //panic!("Crash and burn");
 
@@ -6,11 +7,26 @@ fn main() {
     //v[90];
 
     let f  = File::open("foo");
+    /*
     let f = match f {
         Ok(file) =>  file,
         Err(error) => {
             panic!("failed to open the file {:?}", error)
         } 
     };
+    */
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file {:?}", e),
+            },
+            other_error => panic!("Problem open the file {:?}", other_error)
+        }
+    };
+
+    print!("{:?}", f);
 
 }
