d88721151d0542d10e2ed38e45cafa08531f0a3a
Yaowenqiang <yaowenqiang111@163.com>
2020-03-13 23:34:21 +0800

    add get/set demo

--- redis/src/main.rs
+++ redis/src/main.rs
@@ -1,13 +1,20 @@
 extern crate redis;
-
+use crate::redis::Commands;
 fn main() {
     do_something();
-    println!("Hello, world!");
 }
 
 fn do_something() ->redis::RedisResult<()> {
-    let client = redis::Client::open("redis:://127.0.0.1:6379/")?;
+    let client = match (redis::Client::open("redis://127.0.0.1/")) {
+        Ok(c) => c,
+        Err(e) => panic!("connection error! {}", e)
+    };
+    println!("helo redis");
     let mut con = client.get_connection()?;
-    con.set("my_key", 123);
+    let count : i32 = con.get("a")?;
+    println!("{}", count);
+    let name : String  = con.get("name")?;
+    println!("{}", name);
+    let () = con.set("key1", "value1")?;
     Ok(())
 }
